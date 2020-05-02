c990e43f9f1ea33845c74e89b4ff60bb88421151
unknown <jeffynho32@gmail.com>
2020-05-02 04:54:58 -0300

    routes privates

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '~/pages/SignIn';
 import Deliverymen from '~/pages/Deliverymen';
